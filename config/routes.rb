d92e0c857410792076f0a0ededbd8a381ea449b0
Tchilo <tchiloross@gmail.com>
2022-04-14 17:30:29 +0200

    Update config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :users, only: %i[index]
+  resources :foods, only: %i[index new create destroy]
+
+  resources :recipes, only: %i[index new show create destroy] do
+    resources :recipe_foods, only: %i[new create show destroy]
+  end
+  resources :recipe_foods, only: %i[new new create destroy]
+  resources :public_recipes, only: %i[index]
+  resources :general_shopping_lists, only: %i[index]
+
+  root to: 'users#index'
 end
